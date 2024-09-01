8b94be6e008299885a4e6003cfbf626fe633d2dc
sasha-korn <128995725+sasha-korn@users.noreply.github.com>
2024-09-01 17:11:54 +0000

    Deploying to gh-pages from @ sasha-korn/goit-js-hw-11@007bb752136168584c5b8523bed6ba31987c4870 🚀

--- src/js/pixabay-api.js
+++
@@ -1,3 +0,0 @@
-export function pixabay() {
-  return 'Hello';
-}
--- src/main.js
+++
@@ -1,4 +0,0 @@
-import { render } from './js/render-functions';
-import { pixabay } from './js/pixabay-api';
-
-console.log(render(), pixabay());
