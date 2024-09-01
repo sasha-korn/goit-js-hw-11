007bb752136168584c5b8523bed6ba31987c4870
sasha-korn <korniichukaleksandra@gmail.com>
2024-09-01 15:47:56 +0100

    setting up the project

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,3 @@
+export function pixabay() {
+  return 'Hello';
+}
---
+++ src/main.js
@@ -0,0 +1,4 @@
+import { render } from './js/render-functions';
+import { pixabay } from './js/pixabay-api';
+
+console.log(render(), pixabay());
